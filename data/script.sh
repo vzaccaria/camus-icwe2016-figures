4c4212805450450ec200ce1513ff370503b4ca3c
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2016-01-15 12:50:12 +0100

    minor: add fake data + generated figure

--- data/script.sh
+++ data/script.sh
@@ -1,1 +1,2 @@
-camus-test service-time -u "http://www.google.com" -d ./payload.json -g 100 --get -n 20 > ./data.json
+rm -f ./data.json
+camus-test service-time -u "http://www.google.com" -d ./payload.json -g 100 --get -n 50 > ./data.json
